cef186d65041f757ada50e028606545060caf3a3
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-08 04:27:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,8 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase
-    return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
-  else nil
+  else return "HUH?! SPEAK UP, SONNY!"
   end
 end
